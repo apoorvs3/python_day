47bba5a02a1cdb2717ef4812f70fff184f290c17
apoorvSinha <apoorvs3>
2022-10-01 08:07:44 +0530

    caesar cipher done

---
+++ Day_8/ceaser_cypherv2.py
@@ -0,0 +1,26 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+
+# TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
+def caesar_function(plain_text, shift_amount, direction_shift):
+    message = ''
+    for letter in plain_text:
+        position = alphabet.index(letter)
+        if direction == 'encode':
+            new_position = position + shift_amount
+
+            if new_position > len(alphabet) - 1:  # exception case
+                new_position = shift - (len(alphabet) - position)
+        else:
+            new_position = position - shift_amount
+        new_letter = alphabet[new_position]
+        message += new_letter
+    print(f'The {direction}d text is {message}')
+
+
+caesar_function(text, shift, direction)
---
+++ Day_8/func_with_args.py
@@ -0,0 +1,8 @@
+def greet(name, ID):
+    print(f'Greetings {name} welcome to the game your are player number {ID}')
+    print('Adar Adaab, Abhinandan agaz')
+    print(f'Hola {name} you are player number {ID}')
+
+
+greet('Seong Gi-hun', 456)
+greet(ID=1, name='Oh ||-nam')
