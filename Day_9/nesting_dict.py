3137418ab7bce57f2f8f0100fb53cc8f98ff997d
SINHA <sinhapoo@bcd0746442bd.ant.amazon.com>
2022-10-05 15:24:57 +0530

    day 9 misc

--- Day_9/nesting_dict.py
+++ Day_9/nesting_dict.py
@@ -1,14 +1,27 @@
 capitals = {
-    'france' : 'paris',
+    'france': 'paris',
     'Germany': 'Berlin'
 }
 print(capitals['france'])
 
 # Dictionary in a dictionary
 travel_log = {
-    'France' : {'cities_visited' : ['Paris', 'Lille', 'Dijon'], 'total_visits': 12},
-    'Germany' : { 'Cities_visited' :['Berlin', 'Hamburg', 'Stuttgart'], 'total_visits': 34}
+    'France': {'cities_visited': ['Paris', 'Lille', 'Dijon'], 'total_visits': 12},
+    'Germany': {'Cities_visited': ['Berlin', 'Hamburg', 'Stuttgart'], 'total_visits': 34}
 }
 print(travel_log['Germany'])
 
 # Dictionary in a list
+
+travel_log_1 = {
+    {
+        'country': 'France',
+        'cities_visited': ['Paris', 'Lille', 'Dijon'],
+        'total_visits': 12
+    },
+    {
+        'country': 'Germany',
+        'Cities_visited': ['Berlin', 'Hamburg', 'Stuttgart'],
+        'total_visits': 34
+    }
+}
