b04e964b62d54b7aebff00e29c1ea2d198bfe76b
apoorvSinha <apoorvs3>
2022-10-16 07:37:31 +0530

    snake completed

--- Day_20/snake.py
+++ Day_20/snake.py
@@ -1,5 +1,4 @@
-import time
-from turtle import Turtle, Screen
+from turtle import Turtle
 
 STARTING_POSITION = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
@@ -19,11 +18,17 @@ class Snake:
 
     def create_snake(self):
         for position in STARTING_POSITION:
-            new_segment = Turtle('square')
-            new_segment.color('white')
-            new_segment.penup()
-            new_segment.goto(position)
-            self.segments.append(new_segment)
+            self.add_segment(position)
+
+    def add_segment(self, position):
+        new_segment = Turtle('square')
+        new_segment.color('white')
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
 
     def move(self):
 
