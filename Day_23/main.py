7f8bd1f0d2085ed5d031514a0486c22411a4529f
apoorvSinha <apoorvs3>
2022-10-19 17:30:07 +0530

    Day 23 in prog

---
+++ Day_23/main.py
@@ -0,0 +1,32 @@
+import random
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+player = Player()
+car_manager = CarManager()
+
+# gameplay keys
+screen.listen()
+screen.onkey(player.up, 'Up')
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    car_manager.create_car()
+    car_manager.move_cars()
+
+    # Detect collision with car
+    for car in car_manager.all_cars:
+        if player.distance(car) < 20:
+            print('colise')
+            game_is_on = False
+
+    screen.update()
+
+screen.exitonclick()
