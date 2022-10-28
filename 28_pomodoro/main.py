dd3f07bbca917262935df067e1d86f5f85d2b868
apoorvSinha <apoorvsinha96@gmail.com>
2022-10-28 17:42:22 +0530

    28_oct_mac

--- 28_pomodoro/main.py
+++ 28_pomodoro/main.py
@@ -1,4 +1,6 @@
+import math
 from tkinter import *
+
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -9,11 +11,22 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
-# ---------------------------- TIMER RESET ------------------------------- # 
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+# ---------------------------- TIMER RESET ------------------------------- #
+
+# ---------------------------- TIMER MECHANISM ------------------------------- #
+def start_timer():
+    count_down(5*60)
+
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def count_down(count):
+    count_minute = math.floor(count/60)
+    count_sec = count % 60
+    canvas.itemconfig(timer_text, text=f'{count_minute} : {count_sec}')
+    if count > 0:
+        window.after(1000, count_down, count - 1)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -23,10 +36,10 @@ window.config(padx=100, pady=50, bg=YELLOW)
 timer_label = Label(text='Timer', fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50))
 timer_label.grid(column=1, row=0)
 
-start_button = Button(text='Start', width=7, highlightthickness=0)
+start_button = Button(text='Start', highlightthickness=0, command=start_timer)
 start_button.grid(column=0, row=2)
 
-reset_button = Button(text='Reset', width=7, highlightthickness=0)
+reset_button = Button(text='Reset', highlightthickness=0)
 reset_button.grid(column=2, row=2)
 
 check_mark = Label(text='✓', fg=GREEN, bg=YELLOW, font=(FONT_NAME, 20))
@@ -36,8 +49,8 @@ canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file='tomato.png')
 
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(100, 130, text='00:00', fill='white', font=(FONT_NAME, 35, 'bold'))
-canvas.grid(column=1, row=1)
+timer_text = canvas.create_text(100, 130, text='00:00', fill='white', font=(FONT_NAME, 35, 'bold'))
 
+canvas.grid(column=1, row=1)
 
 window.mainloop()
